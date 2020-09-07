40e3cfce305f462e0e2bcf03f570114f1a302c68
Elijah Morishita <elijah.lm@gmail.com>
2020-09-07 15:49:45 -0500

    3rd commit

--- format_output/average_scores.py
+++ format_output/average_scores.py
@@ -1,20 +1,35 @@
+"""Author: Elijah Morishita
+   elmorishita@dmacc.edu
+   9/7/2020
+   The Average Scores Program
+   """
+
 def names():
+    """ gathers frist/last name as a string and returns a string """
     first_name = input("Please enter your first name: ")
     last_name = input("Please enter you last name: ")
 
-    full_name = first_name + " " + last_name
+    full_name = last_name + "," + first_name + " age:"
     return full_name
 
+def age():
+    """ gathers age and returns an int"""
+    age = int(input("Please enter your age: "))
+    return age
+
 def average():
-    score1 = int(input("Enter your first test score: "))
-    score2 = int(input("Enter your second test score: "))
-    score3 = int(input("Enter your third test score: "))
+    """ gathers score input, calculate an average, and returns an float"""
+    score1 = float(input("Enter your first test score: "))
+    score2 = float(input("Enter your second test score: "))
+    score3 = float(input("Enter your third test score: "))
 
     sum = score1 + score2 + score3
-    ave = sum / 3
+    average_scores = sum / 3
 
-    return ave
+    return average_scores
 
 if __name__ == '__main__':
-    print("Excellent", names(), "now...")
-    print("Your average test score is", average())
+    print(names(), age(), "average grade:", average())
+
+# the input works well unless you enter a char when gathering
+# the score input, it will however accept floats and ints
