c13b2f957d729321a32c0fa68f1e4a3164e4bf78
Elijah Morishita <elijah.lm@gmail.com>
2020-09-07 15:26:58 -0500

    2nd commit

--- format_output/average_scores.py
+++ format_output/average_scores.py
@@ -1,3 +1,20 @@
-def average():
-    pass
+def names():
+    first_name = input("Please enter your first name: ")
+    last_name = input("Please enter you last name: ")
 
+    full_name = first_name + " " + last_name
+    return full_name
+
+def average():
+    score1 = int(input("Enter your first test score: "))
+    score2 = int(input("Enter your second test score: "))
+    score3 = int(input("Enter your third test score: "))
+
+    sum = score1 + score2 + score3
+    ave = sum / 3
+
+    return ave
+
+if __name__ == '__main__':
+    print("Excellent", names(), "now...")
+    print("Your average test score is", average())
--- test/test_average_scores.py
+++ test/test_average_scores.py
@@ -4,5 +4,4 @@ def test_average(self):
     with mock.patch('builtins.input', side_effect=[85,90,95]):
         assert topic2.average() == 90
 
-if __name__ == '__main__':
-    print('PyCharm')
+
