3bc1516357291cc37b21bb6ea4ed52d80a45c96e
Elijah Morishita <elijah.lm@gmail.com>
2020-09-07 15:03:11 -0500

    1st commit

---
+++ test/test_average_scores.py
@@ -0,0 +1,8 @@
+from format_output import average_scores
+
+def test_average(self):
+    with mock.patch('builtins.input', side_effect=[85,90,95]):
+        assert topic2.average() == 90
+
+if __name__ == '__main__':
+    print('PyCharm')
